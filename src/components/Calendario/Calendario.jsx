8f36ea41a8f145eae243b9e569e78f788baf5663
SanguchoMela <melany01-03@hotmail.com>
2024-04-18 01:36:34 -0500

    mostrar todas las citas en el calendario funcional

---
+++ src/components/Calendario/Calendario.jsx
@@ -0,0 +1,45 @@
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/es";
+
+moment.updateLocale("es", {
+  weekdays: "Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado".split("_"),
+  weekdaysShort: "Dom_Lun_Mar_Mié_Jue_Vie_Sáb".split("_"),
+  // weekdaysMin: "Do_Lu_Ma_Mi_Ju_Vi_Sá".split("_"),
+  months:
+    "Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre".split(
+      "_",
+    ),
+  monthsShort: "Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic".split("_"),
+});
+
+const mensajes = {
+  today: "Hoy",
+  previous: "Anterior",
+  next: "Siguiente",
+  month: "Mes",
+  week: "Semana",
+  day: "Día",
+  agenda: "Agenda",
+  date: "Fecha",
+  time: "Hora",
+  event: "Paciente",
+};
+
+// const vistas = ['week','day','agenda']
+
+function Calendario(props) {
+  const localizer = momentLocalizer(moment);
+
+  return (
+    <BigCalendar
+      {...props}
+      localizer={localizer}
+      messages={mensajes}
+      // views={vistas}
+      defaultView="week"
+    />
+  );
+}
+
+export default Calendario;
--- src/paginas/CalendarioCitas.jsx
+++ src/paginas/CalendarioCitas.jsx
@@ -1,11 +1,65 @@
-import BasicCalendar from "../components/Calendario/BasicCalendar";
+import axios from "axios";
+import moment from "moment";
+import { useState, useEffect } from "react";
+import Calendario from "../components/Calendario/Calendario";
 import TitulosOutlet from "../components/TitulosOutlet";
 
 const CalendarioCitas = () => {
+  const [events, setEvents] = useState([]);
+  const [mensaje, setMensaje] = useState({});
+
+  useEffect(() => {
+    const mostrarCitas = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const url = `${import.meta.env.VITE_BACKEND_URL}/citas/mostrar-todas`;
+        const options = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        const response = await axios.get(url, options);
+        const citas = response.data.data;
+
+        const eventosFormateados = citas.map((cita) => ({
+          start: moment(cita.start).toDate(),
+          end: moment(cita.end).toDate(),
+          title: cita.idPaciente,
+        }));
+
+        setEvents(eventosFormateados);
+      } catch (error) {
+        console.log(error);
+        setMensaje({ respuesta: error.response.data.msg, tipo: false });
+        setTimeout(() => {
+          setMensaje({});
+        }, 3000);
+      }
+    };
+    mostrarCitas();
+  }, []);
+
+  const eventos = [
+    {
+      start: moment("2024-04-18T10:00:00").toDate(),
+      end: moment("2024-04-18T11:00:00").toDate(),
+      title: "Juana Martinez",
+    },
+    {
+      start: moment("2024-04-18T11:00:00").toDate(),
+      end: moment("2024-04-18T12:00:00").toDate(),
+      title: "Juan Perez",
+    },
+  ];
+
   return (
     <>
       <TitulosOutlet titulo="Calendario de citas" />
-      <BasicCalendar />
+      <div className="h-5/6">
+        <Calendario events={events} />
+      </div>
     </>
   );
 };
