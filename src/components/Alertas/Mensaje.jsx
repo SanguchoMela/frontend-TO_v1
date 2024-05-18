c25a46a2d7ee4d15c6faa9a40db41ae120c48eb2
SanguchoMela <melany01-03@hotmail.com>
2024-05-17 20:48:25 -0500

    icono de alerta segun tipo

--- src/components/Alertas/Mensaje.jsx
+++ src/components/Alertas/Mensaje.jsx
@@ -1,16 +1,21 @@
 import { TiWarning } from "react-icons/ti";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const Mensaje = ({ children, tipo }) => {
+  const getIcon = () => {
+    switch (tipo) {
+      case true:
+        return <FaCircleCheck className={`w-8 h-8 text-turquesa-fuerte`} />;
+      default:
+        return <TiWarning className={`w-8 h-8 text-naranja`}/>
+    }
+  }
   return (
     <div
       className={`border ${tipo ? "border-turquesa-fuerte" : "border-naranja"} rounded-r-xl flex items-center`}
     >
-      <div
-        className={`border-r p-5 ${tipo ? "border-turquesa-fuerte" : "border-naranja"}`}
-      >
-        <TiWarning
-          className={`w-8 h-8 ${tipo ? "text-turquesa-fuerte" : "text-naranja"}`}
-        />
+      <div className={`border-r p-5 ${tipo ? "border-turquesa-fuerte" : "border-naranja"}`}>
+        {getIcon()}
       </div>
       <div className="mx-3">
         <div
